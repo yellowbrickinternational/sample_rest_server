61435c10d143094d6dff22ce6f10f4adb104a4b1
Bhaal22 <muller.john@gmail.com>
2019-03-22 23:38:59 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+require('console-stamp')(console, '[HH:MM:ss.l]');
+
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post("/pay/ideal", (req, res, next) => {
+    console.log(req.params);
+    console.log(req.body);
+    console.log(req.query);
+    console.log(req.baseUrl);
+
+    res.sendStatus(200);
+});
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
