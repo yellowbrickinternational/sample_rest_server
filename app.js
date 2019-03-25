753be941d4bbb3c9698395db2066ac0046d5d020
Bhaal22 <muller.john@gmail.com>
2019-03-25 09:25:50 +0100

    in memory requests persitence

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ require('console-stamp')(console, '[HH:MM:ss.l]');
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var postRequests = [];
+
+function utcDate() {
+    var dt = new Date();
+    var utcDate = dt.toUTCString();
+
+    return utcDate;
+}
 
 app.get("/", (req, res, next) => {
     console.log('get /');
@@ -13,15 +22,29 @@ app.get("/", (req, res, next) => {
 
 app.get("/pay/ideal", (req, res, next) => {
     console.log('get /pay/ideal/');
-    res.sendStatus(200);
+    res.status(200).json(postRequests);
 });
 
 app.post("/pay/ideal", (req, res, next) => {
-    console.log(req.params);
+    var date = utcDate();
+
+    var timestampedRequest = {
+        'date': date, 
+        'request': {
+            'params': req.params,
+            'body': req.body,
+            'query': req.query,
+            'baseUrl': req.baseUrl
+        }
+    };
+    
+    postRequests.push(timestampedRequest);
+
+
+/*  console.log(req.params);
     console.log(req.body);
     console.log(req.query);
-    console.log(req.baseUrl);
-
+    console.log(req.baseUrl);*/
     res.sendStatus(200);
 });
 
